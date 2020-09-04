f57078bd310fc77911e4074c03cbd9764bab1bba
Martin <martin.stanik.st1@gmail.com>
2020-09-04 12:51:15 +1200

    building algorithm

---
+++ README.md
@@ -0,0 +1,6 @@
+#Task assignment
+
+You have a collection of numeric items among which are numbers '0'. 
+Build the algorithm which shifts all zeros to the end of the array with the best time complexity possible. 
+You are not allowed to use any additional data structures in the solution. 
+Also, keep the items at the same order as they are.
---
+++ src/Solution/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new int[] { 1, 0, 4, 5, 0, 4, 5, 3, 0 };
+
+            // first approach
+
+            var iterations1 = 0;
+            var array = data.ToArray();
+
+            int j = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                ++iterations1;
+                if (array[i] != 0)
+                {
+                    array[j++] = array[i];
+                }
+            }
+
+            while (j < array.Length)
+            {
+                ++iterations1;
+                array[j++] = 0;
+            }
+
+            Console.WriteLine($"1. Approach (keeping the same items order): '{string.Join(",", array)}'");
+            Console.WriteLine($"Time complexity: O({iterations1})");
+            Console.WriteLine(Environment.NewLine);
+
+            // second approach
+
+            var iterations2 = 0;
+
+            array = data.ToArray();
+
+            var end = array.Length - 1;
+            var index = end;
+            for (int i = end; i >= 0; i--)
+            {
+                ++iterations2;
+                if (array[i] == 0)
+                {
+                    var left = array[index];
+                    var right = array[i];
+
+                    array[i] = left;
+                    array[index--] = right;
+                }
+            }
+
+            Console.WriteLine($"2. Approach (changing items order): '{string.Join(",", array)}'");
+            Console.WriteLine($"Time complexity: O({iterations2})");
+            Console.ReadLine();
+        }
+    }
+}
